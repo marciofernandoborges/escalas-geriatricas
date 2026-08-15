976881f3f024a21ad9bd903866ae35d63fcc12d1
marciofernandoborges <marciofborges@gmail.com>
2026-08-15 00:49:25 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'escalas-geriatricas-v5';
+const CACHE = 'escalas-geriatricas-v6';
 const ASSETS = ['./','./index.html','./aga.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
